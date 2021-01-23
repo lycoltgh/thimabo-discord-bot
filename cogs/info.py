da4e4a09824854b5a8f8a720ff6243e42a57e43a
lycoltgh <olliegdownes@outlook.com>
2021-01-23 19:38:43 +0000

    Update

--- cogs/info.py
+++ cogs/info.py
@@ -4,28 +4,27 @@ import random
 from discord import Color as c
 
 teal = c.teal()
-magenta = c.magenta() #choose any colour, but be mindful of the discord.Color colours, they are wierd, unless you go with a custom RGB "system"
+magenta = c.magenta() 
 
 class Load(commands.Cog):
 
     def __init__(self, client):
         self.client = client
 
-
     @commands.command()
-    async def help(self, ctx):
+    async def info(self, ctx):
         em = discord.Embed(
             color=teal
         )
-        em.add_field(name="`Prefix`", value='**My Prefix Is** - <', inline = False)
-        em.add_field(name="Commands", value='Do <Commands to get a list of the finest commands i have to offer.', inline = False)
-        em.add_field(name='Developer', value="Do <dev to see who made me!", inline = False)
+        em.add_field(name="Thimabo's Server!", value='Welcome to thimabos server. Make sure you say hello in general!', inline = False)
+        em.add_field(name="Rules", value='Makes sure you read our `rules` channel.', inline = False)
+        em.add_field(name='What do we do? ', value="Thimabos server is a great place to chill and chat, also provides big giveaways, and downloads!", inline = False)
         em.set_author(name="Information", icon_url="https://cdn.discordapp.com/attachments/787019308260917289/802610114187231242/gwb.gif")
         em.set_thumbnail(url="https://cdn.discordapp.com/attachments/787019308260917289/802610114187231242/gwb.gif")
 
         await ctx.send(embed=em)
 
-print('LOADED')
+
 
 
 
